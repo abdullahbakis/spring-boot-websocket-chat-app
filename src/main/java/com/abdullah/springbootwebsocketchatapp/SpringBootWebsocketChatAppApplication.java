cda76be2260a466891ba1ee4cf441a4e8f4b7601
Abdullah Bakış <abdullahbakis1672@gmail.com>
2022-10-27 00:54:06 +0300

    Update

--- src/main/java/com/abdullah/springbootwebsocketchatapp/SpringBootWebsocketChatAppApplication.java
+++ src/main/java/com/abdullah/springbootwebsocketchatapp/SpringBootWebsocketChatAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootWebsocketChatAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebsocketChatAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebsocketChatAppApplication.class, args);
+    }
 
 }
--- src/main/java/com/abdullah/springbootwebsocketchatapp/controller/ChatController.java
+++ src/main/java/com/abdullah/springbootwebsocketchatapp/controller/ChatController.java
@@ -1,2 +1,27 @@
-package com.abdullah.springbootwebsocketchatapp.controller;public class ChatController {
+package com.abdullah.springbootwebsocketchatapp.controller;
+
+import com.abdullah.springbootwebsocketchatapp.ChatMessagePojo;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessagePojo sendMessage(@Payload ChatMessagePojo chatMessagePojo) {
+        return chatMessagePojo;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessagePojo addUser(@Payload ChatMessagePojo chatMessagePojo, SimpMessageHeaderAccessor headerAccessor) {
+
+// Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessagePojo.getSender());
+        return chatMessagePojo;
+    }
 }
