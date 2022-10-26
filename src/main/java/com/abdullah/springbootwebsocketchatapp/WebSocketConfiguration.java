fb4adfc65bb46aff4cbf206d8de1652be7725032
Abdullah Bakış <abdullahbakis1672@gmail.com>
2022-10-27 00:53:17 +0300

    Initial commit

---
+++ src/main/java/com/abdullah/springbootwebsocketchatapp/WebSocketConfiguration.java
@@ -0,0 +1,2 @@
+package com.abdullah.springbootwebsocketchatapp;public class WebSocketConfiguration {
+}
---
+++ src/main/java/com/abdullah/springbootwebsocketchatapp/controller/ChatController.java
@@ -0,0 +1,2 @@
+package com.abdullah.springbootwebsocketchatapp.controller;public class ChatController {
+}
